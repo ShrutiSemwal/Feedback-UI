50a2aa2b8446ceb97b79ddebf2a5bd5e27be08b0
ShrutiSemwal <semwalshruti450@gmail.com>
2024-02-15 17:38:11 +0530

    Forms, Validation and Animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData); //global state
 
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
@@ -13,10 +15,16 @@ function App() {
     }
   };
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]); //new feedback getting added at top of other feedbacks
+  };
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
