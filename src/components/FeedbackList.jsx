7898d2fca6317cc47fec1098f9c0e09d0c0ac7bf
ShrutiSemwal <semwalshruti450@gmail.com>
2024-02-16 21:20:14 +0530

    ContextAPI

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,11 @@
 import {motion, AnimatePresence} from 'framer-motion'
+import { useContext } from 'react';
 import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types'
+import FeedbackContext from '../context/FeedbackContext';
 
-function FeedbackList({feedback, handleDelete}) {
-    
+function FeedbackList() {
+   const {feedback} = useContext(FeedbackContext) 
+  
   if(!feedback || feedback.length === 0){
    return <p>No feedback yet</p>
   }
@@ -18,7 +20,7 @@ function FeedbackList({feedback, handleDelete}) {
         animate={{opacity: 1}}
         exit={{opacity: 0}}
         >
-            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}  />
+            <FeedbackItem key={item.id} item={item} />
         </motion.div>
            
        ))}
@@ -36,14 +38,14 @@ function FeedbackList({feedback, handleDelete}) {
   // )
 }
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  )
-}
+// FeedbackList.propTypes = {
+//   feedback: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       text: PropTypes.string.isRequired,
+//       rating: PropTypes.number.isRequired,
+//     })
+//   )
+// }
 
 export default FeedbackList
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,9 @@
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({feedback}) {
-   //Calculate ratings avg
+function FeedbackStats() {
+   const {feedback} = useContext(FeedbackContext)
+     //Calculate ratings avg
    let average = feedback.reduce((acc, cur)=>{
       return acc + cur.rating 
    }, 0) / feedback.length
@@ -15,8 +17,8 @@ function FeedbackStats({feedback}) {
   )
 }
 
-FeedbackStats.propTypes = {
-    feedback: PropTypes.array.isRequired,
-}
+// FeedbackStats.propTypes = {
+//     feedback: PropTypes.array.isRequired,
+// }
 
 export default FeedbackStats
